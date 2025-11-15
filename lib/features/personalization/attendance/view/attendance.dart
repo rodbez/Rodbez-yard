62bbfb96390b683734122a9a4cb127fbdf2f5b53
AppDev149 <appdev149@gmail.com>
2025-11-15 19:19:40 +0530

    updated commit

--- lib/features/personalization/attendance/view/attendance.dart
+++ lib/features/personalization/attendance/view/attendance.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:rodbez_yard/features/personalization/attendance/view/widgets/cab_attendance.dart';
 import 'package:rodbez_yard/features/personalization/attendance/view/widgets/driver_attendance.dart';
 import 'package:rodbez_yard/utils/constants/colors.dart';
-
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/device/device_utility.dart';
 
@@ -21,57 +20,57 @@ class Attendance extends StatelessWidget {
           ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
         ),
       ),
-      // body: SafeArea(
-      //   child: DefaultTabController(
-      //     length: 2,
-      //     child: NestedScrollView(
-      //       headerSliverBuilder: (context, _) => [
-      //         SliverToBoxAdapter(
-      //           child: Column(
-      //             children: [
-      //               Padding(
-      //                 padding: EdgeInsetsGeometry.symmetric(
-      //                   vertical: RSizes.spaceBtwItems,
-      //                 ),
-      //                 child: Container(
-      //                   width: RDeviceUtils.getScreenWidth(context) * .75,
-      //                   height: 50,
-      //                   decoration: BoxDecoration(
-      //                     borderRadius: BorderRadius.circular(40),
-      //                     color: const Color(0xFFFFFBE6),
-      //                     border: Border.all(color: const Color(0xFFEDAE10)),
-      //                   ),
-      //                   child: Padding(
-      //                     padding: const EdgeInsets.all(RSizes.xs),
-      //                     child: TabBar(
-      //                       indicator: BoxDecoration(
-      //                         color: const Color(0xFFEDAE10),
-      //                         borderRadius: BorderRadius.circular(40),
-      //                       ),
-      //                       labelColor: RColors.white,
-      //                       unselectedLabelColor: RColors.textDarkGrey,
-      //                       indicatorSize: TabBarIndicatorSize.tab,
-      //                       dividerColor: Colors.transparent,
-      //                       labelStyle: const TextStyle(
-      //                         fontWeight: FontWeight.bold,
-      //                         fontFamily: "Nunito",
-      //                       ),
-      //                       tabs: const [
-      //                         Tab(text: "Driver"),
-      //                         Tab(text: "CAB"),
-      //                       ],
-      //                     ),
-      //                   ),
-      //                 ),
-      //               ),
-      //             ],
-      //           ),
-      //         ),
-      //       ],
-      //       body: TabBarView(children: [DriverAttendance(), CabAttendance()]),
-      //     ),
-      //   ),
-      // ),
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 2,
+          child: NestedScrollView(
+            headerSliverBuilder: (context, _) => [
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsGeometry.symmetric(
+                        vertical: RSizes.spaceBtwItems,
+                      ),
+                      child: Container(
+                        width: RDeviceUtils.getScreenWidth(context) * .75,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40),
+                          color: const Color(0xFFFFFBE6),
+                          border: Border.all(color: const Color(0xFFEDAE10)),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(RSizes.xs),
+                          child: TabBar(
+                            indicator: BoxDecoration(
+                              color: const Color(0xFFEDAE10),
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            labelColor: RColors.white,
+                            unselectedLabelColor: RColors.textDarkGrey,
+                            indicatorSize: TabBarIndicatorSize.tab,
+                            dividerColor: Colors.transparent,
+                            labelStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Nunito",
+                            ),
+                            tabs: const [
+                              Tab(text: "Driver"),
+                              Tab(text: "CAB"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+            body: TabBarView(children: [DriverAttendance(), CabAttendance()]),
+          ),
+        ),
+      ),
     );
   }
 }
